7275e972c283811513815b428387cbe6f7e63320
Jeff Oliver <jeffoliver3384@gmail.com>
2023-04-12 12:30:38 -0400

    Refactor of sanity config file

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,19 +3,24 @@ import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "@/sanity/schemas";
 
+const name = "Dev_Dispatch_Blog_Studio";
+const title = "Dev Dispatch Blog Studio";
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!;
+const basePath = "/studio";
+const plugins = [deskTool(), visionTool()];
+const schema = {
+  types: schemaTypes,
+};
 
 export default defineConfig({
-  name: "Dev_Dispatch_Blog_Studio",
-  title: "Dev Dispatch Blog Studio",
+  name,
+  title,
   projectId,
   dataset,
   apiVersion,
-  basePath: "/studio",
-  plugins: [deskTool(), visionTool()],
-  schema: {
-    types: schemaTypes,
-  },
+  basePath,
+  plugins,
+  schema,
 });
