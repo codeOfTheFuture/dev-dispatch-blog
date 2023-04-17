13f4713d3aba530dd66f785fe709e3b3365f50c9
Jeff Oliver <jeffoliver3384@gmail.com>
2023-04-17 19:04:56 -0400

    Moved tests for homepage into user route group

--- app/(user)/__test__/HomePage.test.tsx
+++ app/(user)/__test__/HomePage.test.tsx
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-
-import HomePage from "@/app/page";
+import HomePage from "@/app/(user)/page";
 
 describe("HomePage", () => {
   it("renders without crashing", () => {
